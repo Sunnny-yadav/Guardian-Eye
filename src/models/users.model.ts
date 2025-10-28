ce20b5ed8e8130a2334fbbac54fa862f8ea08fb8
sunny-yadav <sunny9405683@gmail.com>
2025-10-28 21:04:38 +0530

    refactor(user): modify user schema's password attribute

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -96,7 +96,6 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is required"],
       minlength: [6, "Password must be at least 6 characters"],
-      select: false
     },
   },
   {
