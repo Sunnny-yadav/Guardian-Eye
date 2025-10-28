da7e45335763ca7d4d08332e1561df7bdd573657
sunny-yadav <sunny9405683@gmail.com>
2025-10-28 21:09:04 +0530

    feat(frontend): add UserContext for managing global user state

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import { createContext , ReactNode, useContext, useState} from "react"
+
+interface Location {
+    actual_Address: string;
+    captured_Address: string;
+    latitude: number;
+    longitude: number;
+  };
+  
+  interface User {
+    _id: string;
+    teamName: string;
+    email: string;
+    avatar: string;
+    phone: string;
+    location: Location;
+    rescueBoats: number;
+    ambulances: number;
+    humanRescueTeamSize: number;
+    supplyTrucks: number;
+    createdAt: string;
+    updatedAt: string;
+    [key:string]:any
+  };
+
+  interface UserContextType {
+    user: User | null,
+    logout: () => void,
+    saveUserData: (data: User) => void
+  }
+
+const userContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserContextProvider = ({children} : {children: ReactNode}) => {
+
+    const [user, setUser] = useState<User | null>(null);
+
+    const saveUserData = (data: User) => {
+        setUser(data);
+    };
+
+    const logout = ()=>{
+        setUser(null);
+    };
+
+    
+
+    return (
+        <userContext.Provider value={{user, logout, saveUserData}}>
+            {children}
+        </userContext.Provider>
+    );
+};
+
+export function useUserContext(){
+    const context = useContext(userContext);
+
+    if(context === undefined){
+        throw new Error("useUser must be used within a UserContextProvider")
+    };
+
+    return context;
+}
