f1551dd3f1b7794c2d40088aeb107e4e86631679
sunny-yadav <sunny9405683@gmail.com>
2026-01-25 13:05:29 +0530

    feat(resource-prediction): implement generateDisasterPrediction workflow

--- src/service/generateDisasterPrediction .ts
+++ src/service/generateDisasterPrediction .ts
@@ -4,6 +4,8 @@ import { collectIntensityData } from "./collectIntensityData";
 import { collectResourceData } from "./collectResourceData";
 import { ResourcePlan } from "@/models/resourcePlan.model";
 import { AppError } from "@/helpers/errorHandeller";
+import { intensityInputFormater } from "@/helpers/inputFormater/intensityInput";
+import { resourceInputFormater } from "@/helpers/inputFormater/resourceInput";
 
 interface Coordinates {
   lat: number;
@@ -22,7 +24,35 @@ export async function generateDisasterPrediciton(geoCode: Coordinates) {
     if(!result1.success && "err" in result1){
         throw new Error(result1.err);
     }
-    const intensity_Modle_Input_Values = result1.data;
+
+    // this is used to store in database
+    const intensity_Modle_Input_Values = result1.data; 
+
+    // this is follows the format of input required to the intensity model
+    if (!intensity_Modle_Input_Values) {
+      throw new Error("Intensity data is missing");
+    }
+    const formatedIntensityInput = intensityInputFormater(intensity_Modle_Input_Values);
+
+    console.log("FormatedIntensityInput",formatedIntensityInput)
+    const intensityModelResponse = await fetch("https://ganeshnaik0108-intensity-disaster.hf.space/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formatedIntensityInput),
+    });
+    
+    if (!intensityModelResponse.ok) {
+      const errObj = await intensityModelResponse.json()
+      console.log("Error Obj Of intensity model:",errObj);
+      throw new Error("Intensity Model Failed");
+    }
+    
+    const intensitymodelOutput = await intensityModelResponse.json();
+    
+    console.log("Intensity model Output: ", intensitymodelOutput)
+// todo: here now call a database to capture the historical document with same intensity and name
 
     // call the funciton to collect data for resource model
     const result2 = await collectResourceData(geoCode);
@@ -32,17 +62,61 @@ export async function generateDisasterPrediciton(geoCode: Coordinates) {
     const Resource_Modle_Input_Values = result2?.data?.Resource_Modle_Input_Values;
     const regionId = result2?.data?.regionId;
 
-    console.log(Resource_Modle_Input_Values);
+    if (!Resource_Modle_Input_Values) {
+      throw new Error("Resource data is missing");
+    }
     
+    const formatedResourceInput = {
+      Region: Resource_Modle_Input_Values.point,
+      Population_Density_per_sqkm: Resource_Modle_Input_Values.populationDensity,
+      Total_Population: Resource_Modle_Input_Values.totalPopulation,
+      Road_Density_km_per_sqkm: Resource_Modle_Input_Values.roadDensity,
+      Accessibility_Index: Resource_Modle_Input_Values.accessibilityIndex,
+      Event_Description: "Heavy rainfall warning",
+      Historical_Rescue_Boats: 18.0,
+      Historical_Ambulances: 20.0,
+      Historical_Human_Rescue_Teams: 50.0,
+      Historical_Shelters: 23.0,
+      Historical_Civilians_Evacuated: 20000.0,
+      Historical_Supply_Trucks: 12.0,
+      Historical_Drones: 3.0,
+      Area: Resource_Modle_Input_Values.regionArea,
+      Population_Density: Resource_Modle_Input_Values.populationDensity,
+    };
+    
+    const combinedInput = {
+      ...formatedResourceInput,
+      ...formatedIntensityInput
+    }; 
+
+    console.log("Combine Input resource",combinedInput)
+
+
     // give the collected input to the model we will get the intensity and resource prediction
-    const dummyPredictedResourceAllocation = {
-        rescueBoats: 15,
-        ambulances: 25,
-        humanRescueTeams: 8,
-        shelterCount: 12,
-        civiliansToEvacuate: 5000,
-        supplyTrucks: 30,
-        drones: 10,
+    const response = await fetch("https://ganeshnaik0108-resource-prediction.hf.space/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(combinedInput),
+    });
+    
+    if (!response.ok) {
+      throw new Error("Resource Model Failed");
+    }
+    
+    const modelOutput = await response.json();
+
+    console.log("Resource model Output:", modelOutput)
+
+      const PredictedResourceAllocation = {
+      rescueBoats: Math.round(modelOutput.predicted_resources.Predicted_Rescue_Boats),
+      ambulances: Math.round(modelOutput.predicted_resources.Predicted_Ambulances),
+      humanRescueTeams: Math.round(modelOutput.predicted_resources.Predicted_Human_Rescue_Teams),
+      shelterCount: Math.round(modelOutput.predicted_resources.Predicted_Shelters),
+      civiliansToEvacuate: Math.round(modelOutput.predicted_resources.Predicted_Civilians_To_Evacuate),
+      supplyTrucks: Math.round(modelOutput.predicted_resources.Predicted_Supply_Trucks),
+      drones: Math.round(modelOutput.predicted_resources.Predicted_Drones),
     };
 
     //  create a alert document with session
@@ -50,7 +124,11 @@ export async function generateDisasterPrediciton(geoCode: Coordinates) {
       [{
         climateMetaData: intensity_Modle_Input_Values,
         regionMetaDataId: regionId,
-        intensityPredicted: { flood: "Normal", cyclone: "High", rainfall: "Low" },
+        intensityPredicted: {
+          flood: intensitymodelOutput.Flood_Intensity.includes('Unknown') ? 'Low' : intensitymodelOutput.Flood_Intensity,
+          cyclone: intensitymodelOutput.Cyclone_Intensity.includes('Unknown') ? 'Low' : intensitymodelOutput.Cyclone_Intensity,
+          rainfall: intensitymodelOutput.Rain_Intensity.includes('Unknown') ? 'Low' : intensitymodelOutput.Rain_Intensity
+        }
       }],
       { session }  
     );
@@ -61,7 +139,7 @@ export async function generateDisasterPrediciton(geoCode: Coordinates) {
     // create a resource document with session
     const predictedResourceObj = await ResourcePlan.create(
       [{
-        ...dummyPredictedResourceAllocation,
+        predictedResources: PredictedResourceAllocation,
         alertId: alertObj[0]._id, 
       }],
       { session }
